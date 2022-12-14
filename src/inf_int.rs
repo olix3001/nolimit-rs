d18899ca1ce87828f4c5bc9097eb40339939300f
olix3001 <olimioli9@gmail.com>
2022-12-14 19:18:07 +0100

    feat: basic arithmetic on signed infint

--- src/inf_int.rs
+++ src/inf_int.rs
@@ -2,6 +2,7 @@ use bitvec::vec::BitVec;
 
 use crate::InfUInt;
 use bitvec::prelude::Lsb0;
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 
 #[derive(Clone)]
 pub struct InfInt {
@@ -69,6 +70,77 @@ impl std::fmt::Debug for InfInt {
     }
 }
 
+// ====< Macros >====
+macro_rules! create_assign_operator {
+    ($assign:ty, $assign_method:ident, $method:ident, $rhst:ty) => {
+        impl $assign for InfInt {
+            fn $assign_method(&mut self, rhs: $rhst) {
+                *self = self.clone().$method(rhs);
+            }
+        }
+    };
+    ($assign:ty, $assign_method:ident, $method:ident) => {
+        create_assign_operator!($assign, $assign_method, $method, Self);
+    };
+}
+
+// ====< Arithmetic >====
+impl Add for InfInt {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.sign == rhs.sign {
+            Self {
+                sign: self.sign,
+                value: self.value + rhs.value,
+            }
+        } else {
+            if self.value > rhs.value {
+                Self {
+                    sign: self.sign,
+                    value: self.value - rhs.value,
+                }
+            } else if self.value < rhs.value {
+                Self {
+                    sign: rhs.sign,
+                    value: rhs.value - self.value,
+                }
+            } else {
+                Self::zero()
+            }
+        }
+    }
+}
+create_assign_operator!(AddAssign, add_assign, add);
+
+impl std::ops::Sub for InfInt {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.sign != rhs.sign {
+            Self {
+                sign: self.sign,
+                value: self.value + rhs.value,
+            }
+        } else {
+            if self.value > rhs.value {
+                Self {
+                    sign: self.sign,
+                    value: self.value - rhs.value,
+                }
+            } else if self.value < rhs.value {
+                Self {
+                    sign: !rhs.sign,
+                    value: rhs.value - self.value,
+                }
+            } else {
+                Self::zero()
+            }
+        }
+    }
+}
+create_assign_operator!(SubAssign, sub_assign, sub);
+
 // ====< Comparison >====
 impl PartialEq for InfInt {
     fn eq(&self, other: &Self) -> bool {
@@ -93,5 +165,31 @@ mod tests {
             b,
             InfInt::from_vec(false, vec![true, true, false, true, true, false, true])
         );
+    }
+
+    #[test]
+    fn test_inf_int_add() {
+        let a = infint!(109);
+        let b = infint!(-109);
+        let c = infint!(109);
+        let d = infint!(-109);
+        assert_eq!(a.clone() + b.clone(), InfInt::zero());
+        assert_eq!(a.clone() + c.clone(), infint!(218));
+        assert_eq!(a.clone() + d.clone(), InfInt::zero());
+        assert_eq!(b.clone() + c.clone(), InfInt::zero());
+        assert_eq!(b.clone() + d.clone(), infint!(-218));
+    }
+
+    #[test]
+    fn test_inf_int_sub() {
+        let a = infint!(109);
+        let b = infint!(-109);
+        let c = infint!(109);
+        let d = infint!(-109);
+        assert_eq!(a.clone() - b.clone(), infint!(218));
+        assert_eq!(a.clone() - c.clone(), InfInt::zero());
+        assert_eq!(a.clone() - d.clone(), infint!(218));
+        assert_eq!(b.clone() - c.clone(), infint!(-218));
+        assert_eq!(b.clone() - d.clone(), InfInt::zero());
     }
 }
